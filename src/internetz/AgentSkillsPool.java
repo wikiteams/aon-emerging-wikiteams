a19e48bf60b5745fc4c17ec8441b6e098ff6cf56
Oskar Jarczyk <oskar.jarczyk@pjwstk.edu.pl>
2013-09-07 17:28:15 +0200

    AgentSkillsPool.java and TaskSkillsPool.java refactored

--- src/internetz/AgentSkillsPool.java
+++ src/internetz/AgentSkillsPool.java
@@ -1,29 +1,43 @@
 package internetz;
 
+import internetz.TaskSkillsPool.Method;
+
 import java.util.Random;
 
 public abstract class AgentSkillsPool {
-	
+
+	private enum Method {
+		STATIC_TABLE, LINEAR_KNN, SVM;
+	}
+
 	private static Skill[] skillSet = null;
-	
-	public static void instantiate(){
+
+	public static void instantiate() {
 		say("initialized AgentSkillsPool");
 	}
-	
-	public static Skill chose_random(){
+
+	public static void instantiate(Method method) {
+		say("initialized TaskSkillsPool");
+
+		if (method == Method.STATIC_TABLE) {
+
+		}
+	}
+
+	public static Skill chose_random() {
 		Random generator = new Random();
 		int i = generator.nextInt(skillSet.length);
 		return skillSet[i];
 	}
-	
-	public static void fillWithSkills(Agent agent){
+
+	public static void fillWithSkills(Agent agent) {
 		say("Agent " + agent + " filled with skills");
 	}
-	
-	public static Skill[] get_skill_set(int count){
+
+	public static Skill[] get_skill_set(int count) {
 		return skillSet;
 	}
-	
+
 	private static void say(String s) {
 		PjiitOutputter.say(s);
 	}
--- src/internetz/TaskSkillsPool.java
+++ src/internetz/TaskSkillsPool.java
@@ -3,29 +3,41 @@ package internetz;
 import java.util.Random;
 
 public abstract class TaskSkillsPool {
-	
+
+	public enum Method {
+		STATIC_TABLE, LINEAR_KNN, SVM;
+	}
+
 	private static Skill[] skillSet = null;
-	
-	public static void instantiate(){
+
+	public static void instantiate() {
 		say("initialized TaskSkillsPool");
 	}
-	
-	public static Skill chose_random(){
+
+	public static void instantiate(Method method) {
+		say("initialized TaskSkillsPool");
+
+		if (method == Method.STATIC_TABLE) {
+
+		}
+	}
+
+	public static Skill chose_random() {
 		Random generator = new Random();
 		int i = generator.nextInt(skillSet.length);
 		return skillSet[i];
 	}
-	
-	public static void fillWithSkills(Task task){
+
+	public static void fillWithSkills(Task task) {
 		say("Task " + task + " filled with skills");
 	}
-	
-	public static Skill[] get_skill_set(int count){
+
+	public static Skill[] get_skill_set(int count) {
 		return skillSet;
 	}
-	
+
 	private static void say(String s) {
 		PjiitOutputter.say(s);
 	}
-	
+
 }

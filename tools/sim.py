d8986cccf632d7e8cbee59e0c762f545de53624d
Blazej Gruszka <blazej@gruszka.info>
2014-01-24 17:03:12 +0100

    added segregation of minmax task choice algorithm

--- tools/sim.py
+++ tools/sim.py
@@ -9,24 +9,24 @@ with open('all_simulations.csv', 'rb') as csv_file:
 		(batchNumber, runNumber, tickCount, agentsCount, tasksCount, tasksLeft, experienceDecay, fullyLearnedAgentsLeave, expCutPoint, granularity, granularityType, granularityObstinancy, taskChoiceStrategy, fillAgentSillsMethod, agentSkillPoolDataset, taskSkillPoolDataset, skillChoiceStrategy, taskMinMaxChoice, taskDataSetChecksum, agentDataSetChecksum) = row
 		print tickCount, agentsCount, tasksCount, tasksLeft, experienceDecay, fullyLearnedAgentsLeave, granularity, taskChoiceStrategy, skillChoiceStrategy, taskDataSetChecksum, agentDataSetChecksum
 
-		key = (taskChoiceStrategy, skillChoiceStrategy, agentsCount, tasksCount, experienceDecay, fullyLearnedAgentsLeave, granularity)
+		key = (taskChoiceStrategy, skillChoiceStrategy, agentsCount, tasksCount, experienceDecay, fullyLearnedAgentsLeave, granularity, taskMinMaxChoice)
 		if key not in strategies.keys():
 			strategies[key] = []
 
 		strategies[key].append(float(tickCount)) 
 
 for key in strategies.keys():
-	(taskChoiceStrategy, skillChoiceStrategy, agentsCount, tasksCount, experienceDecay, fullyLearnedAgentsLeave, granularity) = key
+	(taskChoiceStrategy, skillChoiceStrategy, agentsCount, tasksCount, experienceDecay, fullyLearnedAgentsLeave, granularity, taskMinMaxChoice) = key
 	stddevp = numpy.std(strategies[key])
 	average = numpy.average(strategies[key])
 	result = 2 * stddevp / average
 
-	fileName = '%s_%s_agents_%s_tasks_%s_experience_%s_fully_%s_granularity_%s.csv' % key
+	fileName = '%s_%s_agents_%s_tasks_%s_experience_%s_fully_%s_granularity_%s_minmax_%s.csv' % key
 	f = open(fileName, 'a+')
 	f.write("taskChoiceStrategy,skillChoiceStrategy,agentsCount,tasksCount,experienceDecay,fullyLearnedAgentsLeave,granularity,tickCount,taskDataSetChecksum,agentDataSetChecksum\n")
 
 	for row in strategies[key]:
-		f.write("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (taskChoiceStrategy, skillChoiceStrategy, agentsCount, tasksCount, experienceDecay, fullyLearnedAgentsLeave, granularity, row, taskDataSetChecksum, agentDataSetChecksum))
+		f.write("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (taskChoiceStrategy, skillChoiceStrategy, agentsCount, tasksCount, experienceDecay, fullyLearnedAgentsLeave, granularity, taskMinMaxChoice, row, taskDataSetChecksum, agentDataSetChecksum))
 	
 	f.write("ufnosc: %s" % result)
 

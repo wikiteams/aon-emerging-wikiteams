092a7cfc03c5f1e5dbdc1bc0edd0a801c3a45c6f
Oskar Jarczyk <oskar.jarczyk@pjwstk.edu.pl>
2014-01-24 01:25:09 +0100

    Update sim.py
    added new 2 columns, hope it will be working

--- tools/sim.py
+++ tools/sim.py
@@ -6,8 +6,8 @@ strategies = {}
 with open('all_simulations.csv', 'rb') as csv_file:
 	reader = csv.reader(csv_file, delimiter=',')
 	for row in reader:
-		(batchNumber, runNumber, tickCount, agentsCount, tasksCount, tasksLeft, experienceDecay, fullyLearnedAgentsLeave, expCutPoint, granularity, granularityType, granularityObstinancy, taskChoiceStrategy, fillAgentSillsMethod, agentSkillPoolDataset, taskSkillPoolDataset, skillChoiceStrategy, taskMinMaxChoice) = row
-		print tickCount, agentsCount, tasksCount, tasksLeft, experienceDecay, fullyLearnedAgentsLeave, granularity, taskChoiceStrategy, skillChoiceStrategy
+		(batchNumber, runNumber, tickCount, agentsCount, tasksCount, tasksLeft, experienceDecay, fullyLearnedAgentsLeave, expCutPoint, granularity, granularityType, granularityObstinancy, taskChoiceStrategy, fillAgentSillsMethod, agentSkillPoolDataset, taskSkillPoolDataset, skillChoiceStrategy, taskMinMaxChoice, taskDataSetChecksum, agentDataSetChecksum) = row
+		print tickCount, agentsCount, tasksCount, tasksLeft, experienceDecay, fullyLearnedAgentsLeave, granularity, taskChoiceStrategy, skillChoiceStrategy, taskDataSetChecksum, agentDataSetChecksum
 
 		key = (taskChoiceStrategy, skillChoiceStrategy, agentsCount, tasksCount, experienceDecay, fullyLearnedAgentsLeave, granularity)
 		if key not in strategies.keys():
@@ -23,10 +23,10 @@ for key in strategies.keys():
 
 	fileName = '%s_%s_agents_%s_tasks_%s_experience_%s_fully_%s_granularity_%s.csv' % key
 	f = open(fileName, 'a+')
-	f.write("taskChoiceStrategy,skillChoiceStrategy,agentsCount,tasksCount,experienceDecay,fullyLearnedAgentsLeave,granularity,tickCount\n")
+	f.write("taskChoiceStrategy,skillChoiceStrategy,agentsCount,tasksCount,experienceDecay,fullyLearnedAgentsLeave,granularity,tickCount,taskDataSetChecksum,agentDataSetChecksum\n")
 
 	for row in strategies[key]:
-		f.write("%s,%s,%s,%s,%s,%s,%s,%s\n" % (taskChoiceStrategy, skillChoiceStrategy, agentsCount, tasksCount, experienceDecay, fullyLearnedAgentsLeave, granularity, row))
+		f.write("%s,%s,%s,%s,%s,%s,%s,%s\n" % (taskChoiceStrategy, skillChoiceStrategy, agentsCount, tasksCount, experienceDecay, fullyLearnedAgentsLeave, granularity, row, taskDataSetChecksum, agentDataSetChecksum))
 	
 	f.write("ufnosc: %s" % result)
 

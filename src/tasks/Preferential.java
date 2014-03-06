4d14f8a93899bff4bf3fb4dc32ec20b501747eb7
Oskar Jarczyk <oskar.jarczyk@pjwstk.edu.pl>
2014-03-06 01:51:15 +0100

    simple code refactoring in Preferential.java and PersistAdvancement.java

--- src/tasks/Preferential.java
+++ src/tasks/Preferential.java
@@ -11,17 +11,7 @@ import argonauts.PersistAdvancement;
 
 public class Preferential {
 	
-	//private Map<String, Task> tasks;
 	private static final double emptyResultSignal = -0.9;
-	
-	@Deprecated
-	public Preferential(Map<String, Task> tasks){
-		//this.tasks = tasks;
-	}
-	
-	public Preferential() {
-		// Default constructor, no arguments needed
-	}
 
 	public Task concludeMath(Agent agent){
 		Collection<Skill> allAgentSkillsPref = agent.getSkills();
@@ -37,54 +27,5 @@ public class Preferential {
 		}
 		return chosen;
 	}
-	
-	/***
-	 * @Deprecated Reason - very slow execution time, moved to newer method
-	 * @param agent
-	 * @return
-	 */
-//	public Task concludeMath__(Agent agent){
-//		Task mostAdvanced = null;
-//		ArrayList<Task> tasksWithMatchingSkillsPref = new ArrayList<Task>();
-//		Task chosen = null;
-//		
-//		Collection<Skill> allAgentSkillsPref = agent.getSkills();
-//		for (Task singleTaskFromPool : tasks.values()) {
-//			internal:for (Skill singleSkill : allAgentSkillsPref) {
-//				if (singleTaskFromPool.getTaskInternals().containsKey(
-//						singleSkill.toString())) {
-//					tasksWithMatchingSkillsPref.add(singleTaskFromPool);
-//					if (mostAdvanced == null) {
-//						mostAdvanced = singleTaskFromPool;
-//					} else {
-//						if (mostAdvanced.getGeneralAdvance() < singleTaskFromPool
-//								.getGeneralAdvance()) {
-//							mostAdvanced = singleTaskFromPool;
-//						}
-//					}
-//					break internal;
-//				}
-//			}
-//		}
-//
-//		if ( (tasksWithMatchingSkillsPref.size() < 1) || (mostAdvanced == null) ) {
-//			for (Task singleTaskFromPool : tasks.values()) {
-//				if (mostAdvanced == null) {
-//					mostAdvanced = singleTaskFromPool;
-//				} else {
-//					if (mostAdvanced.getGeneralAdvance() < singleTaskFromPool
-//							.getGeneralAdvance()) {
-//						mostAdvanced = singleTaskFromPool;
-//					}
-//				}
-//			}
-//		}
-//		
-//		if (mostAdvanced != null) {
-//			chosen = mostAdvanced;
-//		}
-//		
-//		return chosen;
-//	}
 
 }
